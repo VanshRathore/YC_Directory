6d10afc86029336ab253223ccbf16032192c3734
Vansh Rathore <vanshr083@gmail.com>
2025-04-27 15:12:58 +0530

    added create page and added features in form like validation in form used shadcn@latest add input textarea sonner

---
+++ lib/validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const formSchema = z.object({
+    title : z.string().min(3).max(15),
+    description : z.string().min(20).max(100),
+    category : z.string().min(3).max(15),
+    link: z.string().url().refine( async (url)=>{
+        try{
+            const res = await fetch(url , {method: "HEAD"});
+            const contentType = res.headers.get("content-type");
+
+            return contentType?.startsWith("image/")
+        }catch{
+            return false;
+        }
+    }),
+    pitch : z.string().min(10)
+})
